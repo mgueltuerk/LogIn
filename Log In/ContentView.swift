dcbe70d797b1f9c1c223c0957975f6c5e6fe0e99
Murat Gültürk <m.gueltuerk@gmx.de>
2022-05-04 17:38:14 +0200

    Initial Commit

---
+++ Log In/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Log In
+//
+//  Created by Murat Gültürk on 04.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Log In/Log_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  Log_InApp.swift
+//  Log In
+//
+//  Created by Murat Gültürk on 04.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Log_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
